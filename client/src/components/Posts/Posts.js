0a320a0f9e605cf07dc1490d52af02daef97cb27
kocak-ilyas <62467305+kocak-ilyas@users.noreply.github.com>
2021-02-15 19:19:49 +0300

    Client

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,16 +1,25 @@
-import React from "react";
-import Post from "./Post/Post.js";
-import useStyles from "./styles.js";
+import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
+import { useSelector } from 'react-redux';
 
-const Posts = () => {
+import Post from './Post/Post';
+import useStyles from './styles';
+
+const Posts = ({ setCurrentId }) => {
+  const posts = useSelector((state) => state.posts);
   const classes = useStyles();
 
   return (
-    <div>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </div>
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6} md={6}>
+            <Post post={post} setCurrentId={setCurrentId} />
+          </Grid>
+        ))}
+      </Grid>
+    )
   );
 };
+
 export default Posts;
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,6 +1,5 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from "../constants/actionTypes.js";
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default (posts = [], action) => {
   switch (action.type) {
     case FETCH_ALL:
@@ -17,3 +16,4 @@ export default (posts = [], action) => {
       return posts;
   }
 };
+

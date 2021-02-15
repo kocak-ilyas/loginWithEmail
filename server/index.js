2f1093b8cbbc2f3480224d572cf1f7b66170c83f
kocak-ilyas <62467305+kocak-ilyas@users.noreply.github.com>
2021-02-15 19:26:06 +0300

    server

--- server/index.js
+++ server/index.js
@@ -1,26 +1,30 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-import postRoutes from "./routes/posts.js";
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use("/posts", postRoutes);
+app.use('/posts', postRoutes);
 
-const CONNECTION_URL ="mongodb+srv://cakir:cakir123@cluster0.xxl81.mongodb.net/cakirDB?retryWrites=true&w=majority";
-// "mongodb+srv://cakir:cakir123@cluster0.xxl81.mongodb.net/";
-// "mongodb+srv://cakir:cakir123@cluster0.xxl81.mongodb.net/test";
-const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = 'mongodb+srv://cakir:cakir123@cluster0.xxl81.mongodb.net/cakirDB?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const PORT = process.env.PORT|| 5000;
 
-mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`))
-  )
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set("useFindAndModify", false);
+mongoose.set('useFindAndModify', false);
+
+
+// const CONNECTION_URL ="mongodb+srv://cakir:cakir123@cluster0.xxl81.mongodb.net/cakirDB?retryWrites=true&w=majority";
+// "mongodb+srv://cakir:cakir123@cluster0.xxl81.mongodb.net/";
+// "mongodb+srv://cakir:cakir123@cluster0.xxl81.mongodb.net/test";
